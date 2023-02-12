80d68401a815012b844b67bc56cd0533c3178205
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-02-12 19:25:06 +0100

    feat: authenticate from token via environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export function hello(name: string): string {
-  return `hello, ${name}`
+import { Octokit } from 'octokit'
+
+const auth = process.env.GH_PAT
+
+const octokit = new Octokit({ auth })
+
+async function main() {
+  const {
+    data: { login }
+  } = await octokit.rest.users.getAuthenticated()
+  console.log('Hello, %s', login)
 }
+
+main()
